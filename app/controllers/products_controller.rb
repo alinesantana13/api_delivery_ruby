1b8a675cb0db11f9724666a76457fc51b1a0495f
Aline Santana <aline_santana13@hotmail.com>
2024-03-29 19:11:50 -0300

    Adição dos perfis

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
     before_action :authenticate_user!
     
     def listing
+        if !current_user.admin?
+            redirect_to welcome_path
+        end
     
         @products = Product.includes(:store)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+  enum :role, [:admin, :seller, :buyer]
   has_many :stores
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    return role == "admin"
+  end
+
+  def seller?
+    role == "seller"
+  end
+
+  def buyer?
+    role == "buyer"
+  end
+
 end
