5ea144275c304302c4afb4d229b9643e84f24f6e
Aline Santana <aline_santana13@hotmail.com>
2024-03-29 20:14:34 -0300

    Personalização da view de welcome

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
     before_action :authenticate_user!
+    before_action :check_admin_permission
     
     def listing
         if !current_user.admin?
-            redirect_to welcome_path
+            redirect_to root_path, notice: "No permission for you!"
         end
-    
+        
         @products = Product.includes(:store)
     end
 end
