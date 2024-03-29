456eaa7fbb9ea7b50f463d9482b54690b2821667
Aline Santana <aline_santana13@hotmail.com>
2024-03-29 20:28:28 -0300

    Implementando rotas da api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :stores
   get "listing" => "products#listing"
+
+  post "new" => "registrations#create", as: :create_registration
+  
   root to: "welcome#index"
   
   get "up" => "rails/health#show", as: :rails_health_check
