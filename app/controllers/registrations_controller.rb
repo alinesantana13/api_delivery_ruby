bfefb67d68e4784335e1f71f4c4e15a6304652a9
Aline Santana <aline_santana13@hotmail.com>
2024-04-23 19:48:31 -0300

    Ajustes nos testes e validações na rota new

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,9 +9,19 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: {"email": @user.email}
+        begin
+            @user = User.new(user_params)
+            if @user.save
+                render json: {"email": @user.email}
+            else
+                if @user.errors[:email].include?("has already been taken")
+                    render json: { error: "The email already exists" }, status: :bad_request
+                else
+                    render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+                end
+            end
+        rescue StandardError => e
+            render json: { error: "#{e.message}" }, status: :internal_server_error
         end
     end
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
       email { "example@example.com" }
       password { "123456" }
       password_confirmation { "123456" }
+      role {"seller"}
     end
 end
