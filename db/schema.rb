0a277593ea3945752ad8ae666be7409d805b5ff9
Aline Santana <aline_santana13@hotmail.com>
2024-04-25 21:06:33 -0300

    Criação da model, da migração e dos testes para as credenciais

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_215702) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_100914) do
   create_table "cancel_users", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cancel_users_on_user_id"
+  end
+
+  create_table "credentials", force: :cascade do |t|
+    t.integer "access"
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
