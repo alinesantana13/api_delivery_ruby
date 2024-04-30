21805818bfc580b212583bd47b405b12ce04368a
Aline Santana <aline_santana13@hotmail.com>
2024-04-30 19:59:49 -0300

    feat: Criação do model order

---
+++ app/models/order.rb
@@ -0,0 +1,14 @@
+class Order < ApplicationRecord
+  belongs_to :buyer, class_name: "User"
+  belongs_to :store
+
+  validate :buyer_role
+
+  private
+
+  def buyer_role
+    if !buyer.buyer?
+      errors.add(:buyer, "should be a `user.buyer`")
+    end
+  end
+end
---
+++ db/migrate/20240430221959_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.references :buyer, foreign_key: {to_table: :users}, null: false
+      t.references :store, foreign_key: true, null: false
+      t.string :state, null: false
+      t.timestamps
+    end
+  end
+end
