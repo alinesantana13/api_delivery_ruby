ebe27167e3799b3101c71f7c39e3850fe32fae3e
Aline Santana <aline_santana13@hotmail.com>
2024-04-29 21:12:32 -0300

    test: teste para só exibir uma loja especifica se for passado o token
    - E para fozer o login, também ser passado as credenciais

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -1,11 +1,26 @@
 require "rails_helper"
+
 RSpec.describe "/stores", type: :request do
+
+    let(:user){
+        user = FactoryBot.create(:user)
+        user.save!
+        user
+    }
+
+    let(:credential) { Credential.create_access(:seller )}
+    let(:signed_in) { api_sign_in(user, credential) }
+
     describe "GET /show" do
         it "renders a successful response with stores data" do
-            user = FactoryBot.create(:user)
-            sign_in user
             store = Store.create!(name: "New Store", user: user)
-            get "/stores/#{store.id}", headers: {"Accept" => "application/json"}
+            get(
+                "/stores/#{store.id}", 
+                headers: {
+                    "Accept" => "application/json",
+                    "Authorization" => "Bearer #{signed_in["token"]}"
+                }
+            )
             json = JSON.parse(response.body)
             
             expect(json["name"]).to eq "New Store"
