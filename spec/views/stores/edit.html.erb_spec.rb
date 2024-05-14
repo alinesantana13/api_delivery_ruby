4c213d64a447b671f42ffdc992534e6e3b633f65
Aline Santana <aline_santana13@hotmail.com>
2024-05-14 19:55:10 -0300

    atualização dos testes de views para o edit e new

--- spec/views/stores/edit.html.erb_spec.rb
+++ spec/views/stores/edit.html.erb_spec.rb
@@ -7,16 +7,39 @@ RSpec.describe "stores/edit", type: :view do
     )
   }
 
+  let(:admin) {
+      User.create!(
+        email: "admin@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :admin
+      )
+  }
+
   before(:each) do
     assign(:store, store)
+    @sellers = [
+      User.create(email: "seller1@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :seller), 
+      User.create(
+        email: "seller2@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :seller)]
+    assign(:sellers, @sellers)
   end
 
   it "renders the edit store form" do
+    allow(view).to receive(:current_user).and_return(admin)
     render
 
     assert_select "form[action=?][method=?]", store_path(store), "post" do
 
       assert_select "input[name=?]", "store[name]"
     end
+
+    assert_select "select[name=?]", "store[user_id]"
   end
 end
--- spec/views/stores/new.html.erb_spec.rb
+++ spec/views/stores/new.html.erb_spec.rb
@@ -1,20 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "stores/new", type: :view do
+  let(:admin) {
+      User.create!(
+        email: "admin@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :admin
+      )
+  }
+  
   before(:each) do
     @user = login_user
     
     assign(:store, Store.new(
       name: "MyString", user: @user
     ))
+
+    @sellers = [
+      User.create(email: "seller1@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :seller), 
+      User.create(
+        email: "seller2@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :seller)]
+    assign(:sellers, @sellers)
   end
 
   it "renders new store form" do
+    allow(view).to receive(:current_user).and_return(admin)
     render
 
     assert_select "form[action=?][method=?]", stores_path, "post" do
 
       assert_select "input[name=?]", "store[name]"
     end
+
+    assert_select "select[name=?]", "store[user_id]"
   end
 end
